6858be9aa322cdb434d7d99bc68b0523736e353c
kazakova <kravchenko_sv@inbox.ru>
2020-08-19 11:25:08 +0300

    по 2 уроку, самостоятельно

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
+    meta: { layout: 'main' },
     component: Home
   },
   {
@@ -17,10 +18,46 @@ Vue.use(VueRouter)
     component: () => import('../views/Login.vue')
   },
   {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty'},
+    component: () => import('../views/Register.vue')
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: { layout: 'main'},
     component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: { layout: 'main'},
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main'},
+    component: () => import('../views/Record.vue')
   }
 ]
 
